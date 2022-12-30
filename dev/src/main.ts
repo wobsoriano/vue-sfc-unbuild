0ac654230009a9bebeb89d06e7856c622c3d6e44
wobsoriano <sorianorobertc@gmail.com>
2022-12-29 21:10:07 -0800

    simplify configs

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import MyLibrary from 'my-library'
+import MyLibrary from '../../src'
 import App from './App.vue'
 
 const app = createApp(App)
--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,13 +1,7 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      'my-library': path.resolve(__dirname, '../src'),
-    },
-  },
 })
