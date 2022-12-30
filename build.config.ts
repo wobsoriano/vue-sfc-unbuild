50828fad5d6b89598c6cf85064ba315ad675c04a
wobsoriano <sorianorobertc@gmail.com>
2022-12-29 23:48:05 -0800

    remove cjs support

--- build.config.ts
+++ build.config.ts
@@ -2,7 +2,6 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    { builder: 'mkdist', format: 'cjs', ext: 'cjs', input: './src/' },
     { builder: 'mkdist', input: './src/' },
   ],
   declaration: true,
