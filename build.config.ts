80cdb17ceada3db78d5427d40eadd3117db64426
wobsoriano <sorianorobertc@gmail.com>
2023-07-21 16:24:05 -0700

    bump unbuild to v2 rc

--- build.config.ts
+++ build.config.ts
@@ -3,7 +3,7 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     { builder: 'mkdist', input: './src/' },
-    { builder: 'mkdist', input: './src/', format: 'cjs', ext: 'js' },
+    { builder: 'mkdist', input: './src/', format: 'cjs' },
   ],
   declaration: true,
   clean: true,
