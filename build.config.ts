6291cef64af55a6f61ae7c99009db6be2dcdd92a
wobsoriano <sorianorobertc@gmail.com>
2023-11-07 11:16:40 -0800

    use new eslint flat config and bump deps

--- build.config.ts
+++ build.config.ts
@@ -3,7 +3,8 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     { builder: 'mkdist', input: './src/' },
-    { builder: 'mkdist', input: './src/', format: 'cjs' },
+    // @ts-expect-error: See https://github.com/unjs/unbuild/issues/332
+    { builder: 'mkdist', input: './src/', format: 'cjs', ext: 'cjs' },
   ],
   declaration: true,
   clean: true,
