d656dde9cb7ce862d34d96c8486b474995091b5e
wobsoriano <sorianorobertc@gmail.com>
2023-12-12 13:00:07 -0800

    fix types, bump local deps

--- build.config.ts
+++ build.config.ts
@@ -3,7 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     { builder: 'mkdist', input: './src/' },
-    // @ts-expect-error: See https://github.com/unjs/unbuild/issues/332
     { builder: 'mkdist', input: './src/', format: 'cjs', ext: 'cjs' },
   ],
   declaration: true,
