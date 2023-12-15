a67e299f4c8a42b959282fe91e0007087324b185
wobsoriano <sorianorobertc@gmail.com>
2023-12-14 23:16:20 -0800

    update config

--- build.config.ts
+++ build.config.ts
@@ -2,8 +2,9 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    { builder: 'mkdist', input: './src/' },
-    { builder: 'mkdist', input: './src/', format: 'cjs', ext: 'cjs' },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.ts'], format: 'cjs', loaders: ['js'] },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
   ],
   declaration: true,
   clean: true,
